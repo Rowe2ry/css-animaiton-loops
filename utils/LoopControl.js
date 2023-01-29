6067c21fc6962ce3f3c64566aeb5b0c81d3d323b
Chris Rowe <rowe2ry@yahoo.com>
2023-01-29 14:36:48 -0500

    added all 6 base utility functions and export statement

--- utils/LoopControl.js
+++ utils/LoopControl.js
@@ -15,5 +15,43 @@ function loopManyByTargetClass(animationTime, delayBetween, targetClass, animati
     const elList = document.querySelectorAll(`.${targetClass}`);
     var cycleTime = animationTime;
     elList.forEach(el => el?.classList.toggle(animationClass));
-    setTimeout(() => loopOneByTargetClass(delayBetween, animationTime, targetClass, animationClass), cycleTime);
+    setTimeout(() => loopManyByTargetClass(delayBetween, animationTime, targetClass, animationClass), cycleTime);
 }
+function loopOneNTimesByTargetClass(iterations, animationTime, delayBetween, targetClass, animationClass) {
+    let i = iterations;
+    if (i > 0) {
+        const el = document.querySelector(`.${targetClass}`);
+        var cycleTime = animationTime;
+        el?.classList.toggle(animationClass);
+        --i;
+        setTimeout(() => loopOneNTimesByTargetClass(i, delayBetween, animationTime, targetClass, animationClass), cycleTime);
+    }
+}
+function loopOneNTimesByTargetId(iterations, animationTime, delayBetween, targetId, animationClass) {
+    let i = iterations;
+    if (i > 0) {
+        const el = document.querySelector(`.${targetId}`);
+        var cycleTime = animationTime;
+        el?.classList.toggle(animationClass);
+        --i;
+        setTimeout(() => loopOneNTimesByTargetId(i, delayBetween, animationTime, targetId, animationClass), cycleTime);
+    }
+}
+function loopManyNtimesByTargetClass(iterations, animationTime, delayBetween, targetClass, animationClass) {
+    let i = iterations;
+    if (i > 0) {
+        const elList = document.querySelectorAll(`.${targetClass}`);
+        var cycleTime = animationTime;
+        elList.forEach(el => el?.classList.toggle(animationClass));
+        --i;
+        setTimeout(() => loopManyNtimesByTargetClass(i, delayBetween, animationTime, targetClass, animationClass), cycleTime);
+    }
+}
+module.exports = {
+    loopOneByTargetClass,
+    loopOneByTargetId,
+    loopManyByTargetClass,
+    loopOneNTimesByTargetClass,
+    loopOneNTimesByTargetId,
+    loopManyNtimesByTargetClass
+};
--- utils/LoopControl.ts
+++ utils/LoopControl.ts
@@ -16,5 +16,48 @@ function loopManyByTargetClass (animationTime : number, delayBetween : number, t
     const elList = document.querySelectorAll(`.${targetClass}`);
     var cycleTime : number = animationTime;
     elList.forEach(el => el?.classList.toggle(animationClass));
-    setTimeout(()=>loopOneByTargetClass(delayBetween,animationTime,targetClass,animationClass),cycleTime);
+    setTimeout(()=>loopManyByTargetClass(delayBetween,animationTime,targetClass,animationClass),cycleTime);
 }
+
+function loopOneNTimesByTargetClass (iterations : number, animationTime : number, delayBetween : number, targetClass : string, animationClass: string): void {
+    let i = iterations;
+    if(i > 0) {
+        const el = document.querySelector(`.${targetClass}`);
+        var cycleTime : number = animationTime;
+        el?.classList.toggle(animationClass);
+        --i;
+        setTimeout(()=>loopOneNTimesByTargetClass(i,delayBetween,animationTime,targetClass,animationClass),cycleTime);
+    } 
+}
+
+function loopOneNTimesByTargetId (iterations : number, animationTime : number, delayBetween : number, targetId : string, animationClass: string): void {
+    let i = iterations;
+    if(i > 0) {
+        const el = document.querySelector(`.${targetId}`);
+        var cycleTime : number = animationTime;
+        el?.classList.toggle(animationClass);
+        --i;
+        setTimeout(()=>loopOneNTimesByTargetId(i,delayBetween,animationTime,targetId,animationClass),cycleTime);
+    }
+}
+
+function loopManyNtimesByTargetClass (iterations : number, animationTime : number, delayBetween : number, targetClass : string, animationClass: string): void {
+    let i = iterations;
+    if(i > 0)
+    {
+        const elList = document.querySelectorAll(`.${targetClass}`);
+        var cycleTime : number = animationTime;
+        elList.forEach(el => el?.classList.toggle(animationClass));
+        --i;
+        setTimeout(()=>loopManyNtimesByTargetClass(i,delayBetween,animationTime,targetClass,animationClass),cycleTime);
+    }
+}
+
+module.exports = {
+    loopOneByTargetClass,
+    loopOneByTargetId,
+    loopManyByTargetClass,
+    loopOneNTimesByTargetClass,
+    loopOneNTimesByTargetId,
+    loopManyNtimesByTargetClass
+};
